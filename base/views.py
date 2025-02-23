ae1d0e383dc10715c7318a2a35304462012a66c8
nolanErickson <nericks2@charlotte.edu>
2025-02-23 18:17:58 -0500

    3:01:25 -  Chat Room Messages CRUD
    3:01:25 -  Chat Room Messages CRUD

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.db .models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
@@ -16,12 +17,13 @@ from .forms import RoomForm
 # ]
 
 def loginPage(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -38,12 +40,29 @@ def loginPage(request):
             messages.error(request, 'Username OR password does not exist')
 
 
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registration')
+
+
+    return render(request, 'base/login_register.html', {'form': form})
 
 
 def home(request):
